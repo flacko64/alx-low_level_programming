16f52aa97d6bfac77b16e8b1aa6f7a3bf841df42
flacko64 <luckydube64@gmail.com>
2022-12-13 12:34:01 -0800

    gitty task

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * _isalpha - checks for char alphabet
  * @c: the char to be checked
  *
- * Return: 1 if character is letter lower or upper, otherwise 0
+ * Return: 1 if character is letter lower or upper, 0 otherwise
  */
 int _isalpha(int c)
 {
