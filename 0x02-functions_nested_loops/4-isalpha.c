7847148ae1b034487cd8eb3e232ae17b6a650c3a
flacko64 <luckydube64@gmail.com>
2022-12-13 13:36:06 -0800

    git chmod

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * _isalpha - checks if character is alphabetic
  * @c: the char to be checked
  *
- * Return: 1 if c is a letter, lowercase or uppercase,0 otherwise
+ * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
  */
 int _isalpha(int c);
 {
