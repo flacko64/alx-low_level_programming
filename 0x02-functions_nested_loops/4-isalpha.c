27be3a696f4864c3cd6c8c23884309e04273eee6
flacko64 <luckydube64@gmail.com>
2022-12-13 13:15:52 -0800

    git last task

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,9 +6,9 @@
  *
  * Return: 1 if character is letter lower or upper, 0 otherwise
  */
-int _isalpha(int c)
+int _isalpha(int c);
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && 'Z'));
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && <= 'Z'))
 		return (1);
 	else
 		return (0);
