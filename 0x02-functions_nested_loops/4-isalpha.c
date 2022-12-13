2378dfc18aafb6aaf976957427aa46330d35e232
flacko64 <luckydube64@gmail.com>
2022-12-13 13:32:50 -0800

    commit it

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,9 +4,9 @@
  * _isalpha - checks if character is alphabetic
  * @c: the char to be checked
  *
- * Return: 1 if character is letter lower or upper, 0 otherwise
+ * Return: 1 if c is a letter, lowercase or uppercase,0 otherwise
  */
-int _isalpha(int c)
+int _isalpha(int c);
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && <= 'Z'))
 		return (1);
