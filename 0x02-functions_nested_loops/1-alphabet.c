fc15f007e3b5261f5d4a0188afffa4211defb9c3
flacko64 <luckydube64@gmail.com>
2022-12-13 10:15:32 -0800

    tasking

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - print all alphabet in lowercase
+ * print_alphabet - print the alphabet in lowercase, followed by a new line.
  */
 void print_alphabet(void)
 {
