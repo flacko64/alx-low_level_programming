3dde38f6f154aa3c8e0d065aaf938963cf3f5190
flacko64 <luckydube64@gmail.com>
2022-12-14 15:27:25 -0800

    task 7.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,11 +9,12 @@
 int print_last_digit(int n)
 {
 	int last_digit;
+
 	if (n < 0)
 	{
 		last_digit = (-1 * (n % 10));
 		_putchar (last_digit + '0');
-		return(last_digit);
+		return (last_digit);
 	}
 	else
 	{
