ca996fd4a1eaf3f3f0a670c532026e0457124a27
flacko64 <luckydube64@gmail.com>
2022-12-15 14:09:16 -0800

    task 3

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,15 +10,15 @@
 
 int largest_number(int a, int b, int c);
 {
-int largest;
+	int largest;
 
-if (a > b && a > c)
+if (a > b && b > c)
 {
 	largest = a;
 }
-else if (a > b && c > a)
+else if (b > a && a > c)
 {
-	largest = c;
+	largest = b;
 }
 else if (b > c)
 {
@@ -30,4 +30,5 @@ else
 }
 
 return (largest);
+
 }
