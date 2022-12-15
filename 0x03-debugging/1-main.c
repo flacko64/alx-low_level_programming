d268aa8e896c750446d356eb8725b2b88a71e968
flacko64 <luckydube64@gmail.com>
2022-12-15 10:59:51 -0800

    sir kalkulus is a big jon

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - causes and infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i:
+printf("infinite loop incoming :(\n");
+
+i = 0;
+/*
+ *while (i < 0)
+ *{
+ *putchar(i);
+ *}
+ /
+ printf("infinite loop avoided! \\o\/\n");
+
+ return (0);
+ }
