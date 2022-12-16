2f6ec6d4c2720c6b37a1c79665797a259a8976ce
flacko64 <luckydube64@gmail.com>
2022-12-16 13:28:17 -0800

    task 3333

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,14 +5,12 @@
  * Return: void
  */
 
-void print_numbers(void);
+void print_numbers(void)
 {
 	char c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
-	
-		_putchar(c);
+		_putchar(c)
 	}
 	_putchar ('\n');
-}
