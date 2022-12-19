dad8c306b1c79e7df445e77f88188dbb9fa045d5
flacko64 <luckydube64@gmail.com>
2022-12-19 15:42:26 -0800

    task 4.1

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,12 +11,12 @@ void print_rev(char *s)
 
 	b = 0;
 
-	while (s[n] != '\0')
+	while (s[b] != '\0')
 		b++;
 
 	for (a = b - 1; a >= 0; a--)
 	{
-		_putchar(s[i]);
+		_putchar(s[a]);
 	}
 	_putchar('\n');
 }
