cb6cd8482b648cbbf690320a8b96c9f2171c5e91
flacko64 <luckydube64@gmail.com>
2022-12-19 15:38:00 -0800

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to be printed
+ */
+
+void print_rev(char *s)
+{
+	int a, b;
+
+	b = 0;
+
+	while (s[n] != '\0')
+		b++;
+
+	for (a = b - 1; a >= 0; a--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
