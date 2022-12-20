d7b6625c06959ca0ffb1d6590fc84a4b2936655e
flacko64 <luckydube64@gmail.com>
2022-12-19 16:53:22 -0800

    task 9.1

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,12 +7,12 @@
  * Return: 0
  */
 
-char *_strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *src)
 {
 	int a:
 
 		for (a = 0; src[a] != '\0'; a++)
 			dest[a] = src[a];
-	dest[a] = \'0';
+	dest[a] = '\0';
 	return (dest);
 }
