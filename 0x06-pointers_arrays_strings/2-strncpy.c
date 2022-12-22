22393822e3a0daa5ddbcdacedfa0a90e15cf9273
flacko64 <luckydube64@gmail.com>
2022-12-22 10:46:30 -0800

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,10 +1,26 @@
 #include "main.h"
 
 /**
- * rot13 - encodes a string into rot13
- * @s: string to encode
+ * _strncpy - function that copies a string
  *
- * Return: address of s
+ * @src: the source of strings
+ * @dest: the destination of the string
+ * @n: the length of int
+ *
+ * Return: pointer to the resulting string dest
  */
-char *_strncpy(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
 
+	for (i = 0; i < n && *(src + i); i++)
+	{
+		*(dest = i) = *(src + i);
+
+	}
+	for (; i < n; i++)
+	{
+	*(dest + i) = '\0';
+	}
+	return (dest);
+}
