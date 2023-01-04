4067ecb4a2c6277b62943a272449e1844c657412
flacko64 <luckydube64@gmail.com>
2023-01-04 15:21:03 -0800

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _prints_rev_recursion - prints a string, followed by a new line
+ * _print_rev_recursion - prints a string, followed by a new line
  * @s: pointer to the string
  * Return: void
  */
