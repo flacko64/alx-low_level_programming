ec483ee25b086f230e379026c8b0c18544d961d8
flacko64 <luckydube64@gmail.com>
2023-01-06 17:42:42 -0800

    task 4.1

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,28 +12,25 @@ int main(int argc, char *argv[])
 {
 	int sum = 0, i;
 
-	if (argc > 1)
+	if (argc == 1)
 	{
-		for (i = 1; i < argc; i++)
+		printf("0\n");
+		return 0;
+	}
+	int result = 0;
+	for (int i = 1; i < argc; i++)
+	{
+		char *arg = argv[i];
+		for (int j = 0; arg[j] != '\0'; j++)
 		{
-			int b;
-			char *str;
-
-			str = argv[i];
-			for (b = 0; str[b] != '\0'; b++)
+			if (!isdigit(arg[j]))
 			{
-				if (str[b] < 48 || str[b] > 57)
-				{
-					printf("Error\n");
-					return (1);
-				}
+				printf("Error\n")
+					return 1;
 			}
 		}
+		result += atoi(arg);
 	}
-	for (i = 1; i < argc; i++)
-	{
-		sum += atoi(argv[i]);
-	}
-	ptintf("%d\n", sum);
-	return (0);
+	printf("%d\n", result);
+	return 0;
 }
