bb25324d1c12e0f048a3c1288557c1dbfb2ae57d
flacko64 <luckydube64@gmail.com>
2023-01-06 17:27:03 -0800

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,39 +1,38 @@
-#include <stdio.h>
 #include <stdlib.h>
-
+#include <ctype.h>
+#include <string.h>
 /**
  * main - adds positive numbers
  * @argc: argument count
- * @argv: arguments
+ * @argv: arguments vector
  *
  * Return: 0
  */
-int main(int argc, char **argv[])
+int main(int argc, char *argv[])
 {
-	int i, n, sum = 0;
-	char *flag;
-
-	if (argc < 2)
+	int sum 0, i;
+	if (argc > 1)
 	{
-		printf("0\n");
-		return (0);
+		for (i = 1; i < argc; i++)
+		{
+			int b;
+			char *str;
+
+			str = argv[i];
+			for (b = 0; str[b] != '\0'; b++)
+			{
+				if (str[b] < 48 || str[b] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+		}
 	}
-
-	for (i = 1; argv[i]; i++)
+	for (i = 1; i < argc; i++)
 	{
-		n = strtol(argv[i], &flag, 10);
-		if (*flag)
-		{
-			printf("Error\n");
-			return (1);
-		}
-		else
-		{
-			sum += n;
-		}
-	
-}
-printf("%d\n", sum);
-
-Return (0);
+		sum += atoi(argv[i]);
+	}
+	ptintf("%d\n", sum);
+	return (0);
 }
