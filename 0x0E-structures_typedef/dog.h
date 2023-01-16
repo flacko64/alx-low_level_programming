bb78d1663cbebcecd277f0f3e73a393f43eb196b
flacko64 <luckydube64@gmail.com>
2023-01-16 14:41:42 -0800

    task dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,10 +17,11 @@ struct dog
 	float age;
 	char *owner;
 };
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
-typedef strcut dog dog_t;
+typedef struct dog dog_t;
 
 dog_t *new_dog(char *name, float age, char *owner);
 
