6ea2afd2fa25a48d80460e5274a98ad917990c1b
flacko64 <luckydube64@gmail.com>
2023-01-26 14:03:45 -0800

    task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -25,7 +25,7 @@ int _strlen(const char *s)
  * @str: value to insert into element
  * Return: the number of nodes
  */
-list_t *add_node(list_T **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
 	list_t *add;
 
