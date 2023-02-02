beba6440237a6d4c22ce06fe289f686d196675ba
flacko64 <luckydube64@gmail.com>
2023-02-02 14:31:28 -0800

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -25,22 +25,21 @@ void print_binary(unsigned long int n)
 	unsigned long int dev, result;
 	char flag;
 
-	flag = 0;
-	dev = _power(2, sizeof(unsigned long int) * 8 - 1);
+		flag = 0;
+		dev = _power(2, sizeof(unsigned long int) * 8 - 1);
 
-	while (dev != 0)
-	{
-		result = n & dev;
-		if (result == dev)
+		while (dev != 0)
 		{
-			flag = 1;
-			_putchar('1');
-
+			result = n & dev;
+			if (result == dev)
+			{
+				flag = 1;
+				_putchar('1');
+			}
+			else if (flag == 1 || dev == 1)
+			{
+				_putchar('0');
+			}
+			dev >>= 1;
 		}
-		else if (flag == 1 || dev == 1)
-		{
-			_putchar('0');
-		}
-		dev >>= 1;
-	}
 }
